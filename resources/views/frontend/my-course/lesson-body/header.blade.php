6e9bfb9e81101226c9fb768491acd9ec3f99b0fd
unknown <sudip2tenber@gmail.com>
2024-12-15 13:24:25 +0600

    learning-management

---
+++ resources/views/frontend/my-course/lesson-body/header.blade.php
@@ -0,0 +1,37 @@
+<!-- When there is no desire, all things are at peace. - Laozi -->
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from techydevs.com/demos/themes/html/aduca-demo/aduca/lesson-details.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 14 Nov 2024 11:25:12 GMT -->
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="TechyDevs" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+    <title>Course Details</title>
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com/" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&amp;display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Favicon -->
+    <link rel="icon" sizes="16x16" href="{{ asset('frontend/images') }}/favicon.png" />
+
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/line-awesome.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/animate.min.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/owl.carousel.min.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/select2.min.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/fancybox.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/animated-headline.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/plyr.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/jquery-te-1.4.0.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/css') }}/style.css" />
+    <!-- end inject -->
+  </head>
